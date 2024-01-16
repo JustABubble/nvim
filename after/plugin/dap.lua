90234f0ced93b53a1d0504f056582b4c5815510c
Matthew McElroy <matthew@justabubble.com>
2024-01-16 14:00:25 +1000

    Added C++ debugger support.

---
+++ after/plugin/dap.lua
@@ -0,0 +1,39 @@
+local dap, dapui = require("dap"), require("dapui")
+
+vim.keymap.set("n", "<leader>b", require('dap').toggle_breakpoint)
+vim.keymap.set("n", "<leader>R", require('dap').continue)
+vim.keymap.set("n", ";", require('dap').step_over)
+vim.keymap.set("n", "<leader>;", require('dap').step_into)
+
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
+
+dap.adapters.lldb = {
+  type = "executable",
+  command = "/bin/lldb-vscode-14",
+  name = "lldb"
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    args = {},
+  },
+}
