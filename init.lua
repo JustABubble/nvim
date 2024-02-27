d29b9f9ca642d77bde3b9225e95237aa9c0f6bf3
Matthew McElroy <matthew@justabubble.com>
2024-02-28 08:41:10 +1000

    More rust things

--- init.lua
+++ init.lua
@@ -249,6 +249,12 @@ require('lazy').setup({
     --  Adaparter configuration for specific languages
     { "mfussenegger/nvim-dap-python" },
   },
+
+  -- Language specific plugins
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+  },
 }, {})
 
 -- [[ Setting options ]]
@@ -712,23 +718,65 @@ dap.configurations.cpp = {
     cwd = '${workspaceFolder}',
     stopOnEntry = false,
     args = {},
-
-    -- 💀
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    -- runInTerminal = false,
   },
 }
 
 dap.configurations.c = dap.configurations.cpp
+
+local initCommands = function()
+  local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+  local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+  local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+  local commands = {}
+  local file = io.open(commands_file, 'r')
+
+  if file then
+    for line in file:lines() do
+      table.insert(commands, line)
+    end
+
+    file:close()
+  end
+
+  table.insert(commands, 1, script_import)
+
+  return commands
+end
+
+dap.configurations.rust = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+    initCommands = initCommands,
+  },
+  {
+    name = 'Launch - Debug - Same Name As Directory',
+    type = 'lldb',
+    request = 'launch',
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+    program = function()
+      local current_dir = vim.fn.getcwd()
+      if not current_dir then
+        return ""
+      end
+      local project_name = vim.fs.basename(current_dir)
+
+      return current_dir .. '/target/debug/' .. project_name
+    end,
+    initCommands = initCommands,
+  },
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
